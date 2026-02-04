8c4bf2bef058d34540f4eee0db1fc6cd3822ef2e
l1ph0x (ll) <13763779+l1ph0x@users.noreply.github.com>
2026-02-04 04:57:35 +0400

    Release release/teak.3-ka.4

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,1 +1,17 @@
-export default [];
+import { messages as paragonMessages } from '@openedx/paragon';
+import frMessages from './messages/fr.json';
+import es419Messages from './messages/es_419.json';
+import ruMessages from './messages/ru.json';
+import kaMessages from './messages/ka.json';
+
+const appMessages = {
+  fr: frMessages,
+  'es-419': es419Messages,
+  ru: ruMessages,
+  ka: kaMessages,
+};
+
+export default [
+  paragonMessages,
+  appMessages,
+];
